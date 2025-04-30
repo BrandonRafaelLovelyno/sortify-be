cad96b3c7062f621cd62f2395ef501e920a87dd4
Brandon <brandonrafael80@gmail.com>
2025-04-30 08:13:46 +0700

    login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignUpDto } from './auth.dto';
+import { SignInDto, SignUpDto } from './auth.dto';
+import { VerifyUserDto } from './auth.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -12,4 +14,19 @@ export class AuthController {
     await this.authService.signUp(body);
     return { message: 'Verification email sent successfully' };
   }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('verify-user')
+  async verifyUser(@Body() body: VerifyUserDto) {
+    const user = await this.authService.verifyUser(body);
+    return { message: 'User verified successfully', user };
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(@Body() body: SignInDto, @Res() response: Response) {
+    const user = await this.authService.login(body, response);
+    response.status(HttpStatus.OK).json({ message: 'Login successful', user });
+  }
+
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AppMailerModule } from 'src/mailer/mailer.module';
 import { CommonModule } from 'src/common/common.module';
+import { AuthHelper } from './auth.helper';
 
 @Module({
   imports: [PrismaModule, AppMailerModule, CommonModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthHelper],
 })
 export class AuthModule {}
