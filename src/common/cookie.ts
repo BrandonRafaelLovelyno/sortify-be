e3498bf9e55bb7b1bc28fc0018565b356625d2da
Brandon <brandonrafael80@gmail.com>
2025-05-14 00:08:00 +0700

    allow cross-site cookie

--- src/common/cookie.ts
+++ src/common/cookie.ts
@@ -7,7 +7,7 @@ export class Cookie {
     response.cookie(tokenName, token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
   }
