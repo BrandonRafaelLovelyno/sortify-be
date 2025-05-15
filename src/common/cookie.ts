dbc889077faca35fff8acff5f015236afbfd4e9c
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2025-05-16 01:49:33 +0700

    set cookie using set header

--- src/common/cookie.ts
+++ src/common/cookie.ts
@@ -1,15 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Response } from 'express';
 
+
 @Injectable()
 export class Cookie {
   sendToken(response: Response, tokenName: string, token: string) {
-    response.cookie(tokenName, token, {
-      httpOnly: false,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'none',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+    const isProd = process.env.NODE_ENV === 'production';
+
+    const cookie = `${tokenName}=${token}; Path=/; Max-Age=${7 * 24 * 60 * 60}; SameSite=None; Secure; ${isProd ? '' : ''} HttpOnly; Partitioned`;
+
+    response.setHeader('Set-Cookie', cookie);
   }
 
   getCookie(request: any, tokenName: string) {
@@ -20,3 +20,23 @@ export class Cookie {
     return token;
   }
 }
+
+// @Injectable()
+// export class Cookie {
+//   sendToken(response: Response, tokenName: string, token: string) {
+//     response.cookie(tokenName, token, {
+//       httpOnly: false,
+//       secure: process.env.NODE_ENV === 'production',
+//       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'none',
+//       maxAge: 7 * 24 * 60 * 60 * 1000,
+//     });
+//   }
+
+//   getCookie(request: any, tokenName: string) {
+//     const token = request.cookies?.[tokenName];
+//     if (!token) {
+//       throw new UnauthorizedException('No token provided');
+//     }
+//     return token;
+//   }
+// }
