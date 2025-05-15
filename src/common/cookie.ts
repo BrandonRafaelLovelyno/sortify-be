22b462193e5dc132f215ee4e1e56fab3c7f70cd1
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2025-05-16 03:24:43 +0700

    hard code cookie

--- src/common/cookie.ts
+++ src/common/cookie.ts
@@ -6,8 +6,8 @@ export class Cookie {
   sendToken(response: Response, tokenName: string, token: string) {
     response.cookie(tokenName, token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: true,
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
   }
